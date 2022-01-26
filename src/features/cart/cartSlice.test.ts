77458195c42c0ab0ae3004e3e096e3f47b7b6480
David Lee <davidheunlee@gmail.com>
2022-01-26 11:49:13 -0500

    completed testing-8

--- src/features/cart/cartSlice.test.ts
+++ src/features/cart/cartSlice.test.ts
@@ -3,7 +3,9 @@ import cartReducer, {
   addToCart,
   removeFromCart,
   updateQuantity,
+  getNumItems,
 } from './cartSlice';
+import type { RootState } from '../../app/store';
 
 describe('cart reducer', () => {
   //test.todo('an empty action');
@@ -64,6 +66,28 @@ describe('cart reducer', () => {
       checkoutState: 'READY',
       errorMessage: '',
       items: { abc: 1, def: 5 },
+    });
+  });
+});
+describe('selectors', () => {
+  describe('getNumItems', () => {
+    it('should return 0 with no items', () => {
+      const cart: CartState = {
+        checkoutState: 'READY',
+        errorMessage: '',
+        items: {},
+      };
+      const result = getNumItems({ cart } as RootState);
+      expect(result).toEqual(0);
+    });
+    it('should add up the total', () => {
+      const cart: CartState = {
+        checkoutState: 'READY',
+        errorMessage: '',
+        items: { abc: 3, def: 3 },
+      };
+      const result = getNumItems({ cart } as RootState);
+      expect(result).toEqual(6);
     });
   });
 });
